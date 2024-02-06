d2a05db6f97c1c17e29ae32faf682711ecf40ffd
Wangari-j <wangarijudy@gmail.com>
2024-02-06 21:54:46 +0300

    conditions

---
+++ conditions.py
@@ -0,0 +1,12 @@
+city = "Nairobi"
+
+if city == "Laikipia":
+   print("Nanyuki")
+elif city == "TransNzoia":
+    print("Kitale")
+elif city == "Kilif":
+    print("Watamu")
+
+else:
+    print("Kisumu")
+    
